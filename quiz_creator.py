9ab158a7969e0d2cafadff4d2be75430904a707f
jadensanares <sanaresj@gmail.com>
2025-04-10 09:33:29 +0800

    created the pseudocode guide for the program

---
+++ quiz_creator.py
@@ -0,0 +1,6 @@
+# Assignment 9: Quiz Creator
+# Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
+# Hint: 
+# On the next assignment, you will create the Quiz program that read the output file of the Quiz Creator. The user will answer the randomly selected question and check if the answer is correct.
+# Format the data written on the text file so it is easier the read on the next assignment.
+# Feel free to use any library
