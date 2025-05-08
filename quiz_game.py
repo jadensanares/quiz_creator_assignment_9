a0a7ace19c6e4282d8a55eaf7905ac7494f9995a
jadensanares <sanaresj@gmail.com>
2025-05-09 07:36:55 +0800

    added first part of the pseudocode for the program

---
+++ quiz_game.py
@@ -0,0 +1,28 @@
+# For Assignment 10: Quiz
+# Create the Quiz program that read the output file of the Quiz Creator.
+# The user will answer the randomly selected question and check if the answer is correct
+# ////=======================================================================================///
+
+# INITIALIZE the QUIZ Game
+
+
+# DISPLAY the welcome message to the players
+
+
+# ASK the user to inpute filename of quiz (its default is set to "created_quiz_questions.txt")
+
+
+# CALL function to load up the questions form the created text file
+#      OPEN file in read mode
+#      READ all lines
+#      INITIALIZE an empoty list to store each question set
+#      FOR each line group (question + choices + correct answer)
+#           break down the text and extract:
+#             Question, Choices (a, b , c, d), Correct Answer
+#           ADD each question set to the list
+#      RETURN list of break downed txt questions
+
+# IF no questions were found
+#   DISPLAY an error message
+#   END program
+
